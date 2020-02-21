82515a71a7a74d98f28588eefe1bc96d925eb2de
Caleb Pleasant <calebpleasant@gmail.com>
2020-02-21 08:17:41 -0800

    Updated Game with Scavange Methods and Map View

---
+++ Planet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Red_Federation_Space_Game
+{
+    public class Planet
+    {
+        //Planet attributes
+        public string name;
+        public int planetX;
+        public int planetY;
+
+        //Planet constructor
+        public Planet(string name, int x, int y)
+        {
+            this.name = name;
+            this.planetX = x;
+            this.planetY = y;
+        }
+    }//End Planet Class
+}
